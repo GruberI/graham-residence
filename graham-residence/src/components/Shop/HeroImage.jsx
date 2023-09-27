5530ccfde3fb2009d6caa92d7473efb20e45c555
Issa Gruber <issa.gruber88@gmail.com>
2023-09-27 12:02:08 -0400

    banner image cover, removed space at sides of nav, image and content. Removed padding below shop nav

--- graham-residence/src/components/Shop/HeroImage.jsx
+++ graham-residence/src/components/Shop/HeroImage.jsx
@@ -3,10 +3,8 @@ export default function HeroImage() {
     <div className="relative isolate overflow-hidden">
       <img
         src="https://ipfs.filebase.io/ipfs/QmfWF5UMbrXZH72P7nkxvCvywpjUwS6qpxoAQMKa1dc182"
-        alt=""
-        className="mx-auto max-w-7xl h-full"
-        // **full screen image**
-        // className="w-screen h-screen"
+        alt="hero_image_graham_residence_livingroom"
+        className="w-screen h-screen object-cover "
       />
     </div>
   );
--- graham-residence/src/components/Shop/Navigation.jsx
+++ graham-residence/src/components/Shop/Navigation.jsx
@@ -17,11 +17,11 @@ const navigation = {
 
 export default function Navigation() {
   return (
-    <div aria-label="Top" className="w-screen">
-      <div className="bg-white px-6 py-24 sm:py-8 lg:px-8">
+    <div aria-label="Top" className="w-screen sm:px-0 lg:px-0">
+      <div className="bg-white">
         <div className="mx-auto text-center">
-          <div className="inset-x-0 bottom-0 sm:static sm:flex-1 sm:self-stretch">
-            <div className="flex h-14 space-x-8 overflow-x-auto  px-4 pb-px sm:h-full sm:justify-center sm:overflow-visible border-t border-b sm:border-t-2 sm:border-b-2 sm:pt-3 sm:pb-3">
+          <div className="inset-x-0 bottom-0 sm:static sm:flex-1 sm:self-stretch ">
+            <div className="flex h-14 space-x-8 overflow-x-auto  px-4 pb-px sm:h-full sm:justify-center sm:overflow-visible border-t  sm:border-t-2 sm:pt-3 sm:pb-3">
               {navigation.categories.map((item) => (
                 <a
                   key={item.name}
